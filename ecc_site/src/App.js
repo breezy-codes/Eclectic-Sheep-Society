4116516c7bdb8e53ccd0f8c4f8687355bbb2b971
Larsa-mrp <bradleyspiclin@gmail.com>
2023-08-19 16:15:08 +1000

    added firebase and poll functions

--- ecc_site/src/App.js
+++ ecc_site/src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './components/NavBar/NavBar';
-import Footer from './components/Footer';
-import Home from './components/HomePage/Home';
-import Quiz from './components/quiz/Quiz';
-import Stats from './components/stats/Stats';
-import Risks from './components/risks/Risks';
-import FAQ from './components/faq/FAQ';
-import UserStories from './components/userStories/UserStories';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer";
+import Home from "./components/HomePage/Home";
+import Quiz from "./components/quiz/Quiz";
+import Stats from "./components/stats/Stats";
+import Risks from "./components/risks/Risks";
+import FAQ from "./components/faq/FAQ";
+// import UserStories from "./components/userStories/UserStories";
 
 const App = () => {
   return (
@@ -16,7 +16,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
-        <Route path="/UserStories" element={<UserStories />} />
+        {/* <Route path="/UserStories" element={<UserStories />} /> */}
         <Route path="/Quiz" element={<Quiz />} />
         <Route path="/Risks" element={<Risks />} />
         <Route path="/Stats" element={<Stats />} />
---
+++ ecc_site/src/firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "talktipsy-861d9.firebaseapp.com",
+  projectId: "talktipsy-861d9",
+  storageBucket: "talktipsy-861d9.appspot.com",
+  messagingSenderId: "730803157447",
+  appId: "1:730803157447:web:744c5ac14602c3ccef41e2",
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(firebaseApp);
