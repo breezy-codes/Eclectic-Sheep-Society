b256270ec9664963dc1412594f9f5e31b388a044
BRIANNA <BREEZYCODES@GMAIL.COM>
2023-08-20 09:54:37 +1000

    added new website

--- ecc_site/src/components/CrisisBox/CrisisBox.js
+++ ecc_site/src/components/CrisisBox/CrisisBox.js
@@ -6,7 +6,7 @@ const CrisisBox = () => {
     <div className="crisis-box">
       <h2 className="title">Help is Available</h2>
       <p className="text">
-        If you're in crisis or need support, <br></br> please contact Lifelinegg.
+        If you're in crisis or need support, <br></br> please contact Lifeline.
       </p>
       <p className="phone">Phone: <strong>13 11 14</strong></p>
       <div className="button-container">
