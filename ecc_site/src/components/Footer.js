f9f7eb288ab82bd573f5ad5c291e7eff67f754cb
BRIANNA <BREEZYCODES@GMAIL.COM>
2023-08-19 19:16:36 +1000

    Revert "Revert "Revert "revert"""
    This reverts commit 1581b6370c403e31a886ac73a39b13546676bfbc.

--- ecc_site/src/components/Footer.js
+++ ecc_site/src/components/Footer.js
@@ -1,50 +1,26 @@
+// Footer.js
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
-  const footerStyle = {
-    backgroundColor: 'rgba(14, 165, 197)',
-    color: 'white',
-    textAlign: 'center',
-    padding: '2rem 0',
-  };
-
-  const contentStyle = {
-    maxWidth: '800px',
-    margin: '0 auto',
-  };
-
-  const paragraphStyle = {
-    marginBottom: '1.5rem', // Update the margin for line spacing
-  };
-
-  const copyrightStyle = {
-    fontSize: '0.9rem',
-    opacity: 0.7,
-  };
-
   return (
-    <footer style={footerStyle}>
-      <div style={contentStyle}>
-        <p style={paragraphStyle}>
+    <footer className="footer">
+      <div className="content">
+        <p className="paragraph">
           Untold Story, created as part of the 2023 GovHack competition by the Eclectic Sheep Society.
         </p>
-        <p style={paragraphStyle}>
+        <p className="paragraph">
           Project Team - Andrew Paull, Brad Splicin, Brianna Laird, Anastasiya Guenov and Rishant Sharma
         </p>
-        <p style={paragraphStyle}>
+        <p className="paragraph">
           <b>Team Leader</b> - Anastasiya Guenov<br></br>
           <b>Website & Frontend Developer</b> - Brianna Laird<br></br>
           <b>Data Analyst & AI Model</b> - Rishant Sharma<br></br>
           <b>Backend Developer</b> - Brad Splicin<br></br>
           <b>Presentation</b> - Andrew Paull<br></br>
         </p>
-<<<<<<< HEAD
         <p className="copyright">
           &copy; 2023 Eclectic Sheep Society Be herd. Be eclectic.
-=======
-        <p style={copyrightStyle}>
-          &copy; 2023 Eclectic Sheep Society
->>>>>>> parent of c894553 (Updated css colours)
         </p>
       </div>
     </footer>
