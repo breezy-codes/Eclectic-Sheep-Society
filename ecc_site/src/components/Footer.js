917ddf5b5ba91476425c0088c9ec359e59bdb19a
BRIANNA <BREEZYCODES@GMAIL.COM>
2023-08-19 19:15:34 +1000

    revert

--- ecc_site/src/components/Footer.js
+++ ecc_site/src/components/Footer.js
@@ -1,26 +1,50 @@
-// Footer.js
 import React from 'react';
-import './Footer.css';
 
 const Footer = () => {
+  const footerStyle = {
+    backgroundColor: 'rgba(14, 165, 197)',
+    color: 'white',
+    textAlign: 'center',
+    padding: '2rem 0',
+  };
+
+  const contentStyle = {
+    maxWidth: '800px',
+    margin: '0 auto',
+  };
+
+  const paragraphStyle = {
+    marginBottom: '1.5rem', // Update the margin for line spacing
+  };
+
+  const copyrightStyle = {
+    fontSize: '0.9rem',
+    opacity: 0.7,
+  };
+
   return (
-    <footer className="footer">
-      <div className="content">
-        <p className="paragraph">
+    <footer style={footerStyle}>
+      <div style={contentStyle}>
+        <p style={paragraphStyle}>
           Untold Story, created as part of the 2023 GovHack competition by the Eclectic Sheep Society.
         </p>
-        <p className="paragraph">
+        <p style={paragraphStyle}>
           Project Team - Andrew Paull, Brad Splicin, Brianna Laird, Anastasiya Guenov and Rishant Sharma
         </p>
-        <p className="paragraph">
+        <p style={paragraphStyle}>
           <b>Team Leader</b> - Anastasiya Guenov<br></br>
           <b>Website & Frontend Developer</b> - Brianna Laird<br></br>
           <b>Data Analyst & AI Model</b> - Rishant Sharma<br></br>
           <b>Backend Developer</b> - Brad Splicin<br></br>
           <b>Presentation</b> - Andrew Paull<br></br>
         </p>
+<<<<<<< HEAD
         <p className="copyright">
           &copy; 2023 Eclectic Sheep Society Be herd. Be eclectic.
+=======
+        <p style={copyrightStyle}>
+          &copy; 2023 Eclectic Sheep Society
+>>>>>>> parent of c894553 (Updated css colours)
         </p>
       </div>
     </footer>
