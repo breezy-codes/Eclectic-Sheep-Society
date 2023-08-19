ce0eff5adbd5128e1299c67cccf948b46788283f
BRIANNA <BREEZYCODES@GMAIL.COM>
2023-08-19 21:16:34 +1000

    Update App.js

--- ecc_site/src/App.js
+++ ecc_site/src/App.js
@@ -1,4 +1,15 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer';
+import Home from './components/HomePage/Home';
+import Quiz from './components/quiz/Quiz';
+import Stats from './components/stats/Stats';
+import Risks from './components/risks/Risks';
+import FAQ from './components/faq/FAQ';
+import UserStories from './components/userStories/UserStories';
+import DataCollection from './components/DataCollection/DataCollection';
+import CrisisBox from './components/CrisisBox/CrisisBox';
 
 const App = () => {
   return (
@@ -9,10 +20,10 @@ const App = () => {
         <Route path="/Home" element={<Home />} />
         <Route path="/UserStories" element={<UserStories />} />
         <Route path="/Quiz" element={<Quiz />} />
-        <Route path="/DataCollection" element={<DataCollection />} />
+        <Route path="/DataCollection" element={<DataCollection />} />      
         <Route path="/Risks" element={<Risks />} />
         <Route path="/Stats" element={<Stats />} />
-        <Route path="/FAQQ" element={<FAQ />} />
+        <Route path="/FAQ" element={<FAQ />} />
       </Routes>
       <CrisisBox />
       <Footer />
