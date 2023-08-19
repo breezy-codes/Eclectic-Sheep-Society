6d5c5331c60d8710ceaafc0ffe8e8bb0d345dd07
BRIANNA <BREEZYCODES@GMAIL.COM>
2023-08-19 21:06:50 +1000

    navbar changes

--- ecc_site/src/components/NavBar/NavBar.js
+++ ecc_site/src/components/NavBar/NavBar.js
@@ -2,16 +2,19 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './NavBar.css'; // Make sure to import your custom CSS file
+import OurLogo from './OurLogo.png';
 
 const NavBar = () => {
   return (
     <>
       <Navbar className="custom-navbar" expand="lg">
         <Container>
-          <Navbar.Brand href="/home" className="navbar-brand">Untold Story</Navbar.Brand>
+          <Navbar.Brand href="/home" className="navbar-brand">
+            <img src={OurLogo} alt="Our Logo" className="logo" />
+          </Navbar.Brand>
           <Nav className="navbar-nav">
             <Nav.Link href="/Home">Home</Nav.Link>
-            <Nav.Link href="/DataCollection">Help Us</Nav.Link>            
+            <Nav.Link href="/DataCollection">Help Us</Nav.Link>
             <Nav.Link href="/UserStories">User Stories</Nav.Link>
             <Nav.Link href="/Chat">Chatbot</Nav.Link>
             <Nav.Link href="/Quiz">Do you Know Aussies?</Nav.Link>
