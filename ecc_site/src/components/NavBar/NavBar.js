bdbdb22e2e0220f988aed7655d47789a60a76e58
BRIANNA <BREEZYCODES@GMAIL.COM>
2023-08-20 15:12:39 +1000

    update to app

--- ecc_site/src/components/NavBar/NavBar.js
+++ ecc_site/src/components/NavBar/NavBar.js
@@ -7,18 +7,17 @@ import './NavBar.css'; // Import your custom CSS file
 const NavBar = () => {
   return (
     <Navbar expand="lg" className="custom-navbar">
-      <Navbar.Brand as={Link} to="/home"> {/* Use Link instead of href */}
+      <Navbar.Brand as={Link} to="/Home"> {/* Update to="/Home" */}
         <img src={OurLogo} alt="Logo" className="logo" />
       </Navbar.Brand>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link as={Link} to="/home" className="nav-link">Home</Nav.Link> {/* Use Link instead of href */}
+          <Nav.Link as={Link} to="/Home" className="nav-link">Home</Nav.Link>
           <Nav.Link as={Link} to="/DataCollection" className="nav-link">Help Us</Nav.Link>
-          <Nav.Link as={Link} to="/UserStories" className="nav-link">User Stories</Nav.Link>
-          <Nav.Link as={Link} to="/Chat" className="nav-link">Chatbot</Nav.Link>
+          <Nav.Link as={Link} to="/userStories" className="nav-link">User Stories</Nav.Link>
           <Nav.Link as={Link} to="/Quiz" className="nav-link">Do you Know Aussies?</Nav.Link>
           <Nav.Link as={Link} to="/Stats" className="nav-link">Stats</Nav.Link>
-          <Nav.Link as={Link} to="/FAQ" className="nav-link">FAQ</Nav.Link>
+          <Nav.Link as={Link} to="/faq" className="nav-link">FAQ</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
